e036fd71014c621305e3a3f4ffb68c31a0bade65
Max Mauermann <max-mauermann@web.de>
2018-03-28 20:07:47 +0200

    CNN model added

--- src/KerasModel.py
+++ src/KerasModel.py
@@ -28,10 +28,28 @@ def nn_Model(output_shape):
     model.add(Dense(output_shape, activation='sigmoid'))
     return model
 
+def cnn_Model(output_shape):
+    model = Sequential()
+    model.add(Conv2D(input_shape=(33, 64, 64),
+                     filters=64,
+                     activation='relu',
+                     kernel_size=(10, 10),
+                     strides=(2, 2),
+                     data_format='channels_first'))
+    model.add(Dropout(rate=0.5))
+    model.add(AveragePooling2D(pool_size=(9,9),
+                               data_format='channels_first'))
+    model.add(Flatten())
+    model.add(Dense(units=output_shape, activation='sigmoid'))
+    return model
+
+
 def run_Model():
     x_text = np.load(data_paths.x_text)
+    x_img = np.load(data_paths.x_img)
     y = np.load(data_paths.y)
 
+    x_train = x_img
     species_count = y.shape[1]
     print(species_count)
 
@@ -40,12 +58,13 @@ def run_Model():
     top50_acc = functools.partial(top_k_categorical_accuracy, k=50)
     top50_acc.__name__ = 'top50_acc'
 
-    model = nn_Model(species_count)
+    #model = nn_Model(species_count)
+    model = cnn_Model(species_count)
     model.compile(optimizer='sgd',
                   loss='mse',
-                  metrics=[top3_acc,  top50_acc, 'accuracy'])
+                  metrics=['accuracy', top3_acc, top50_acc])
 
-    x_train, x_valid, y_train, y_valid = train_test_split(x_text, y, test_size=settings.train_val_split, random_state=settings.seed)
+    x_train, x_valid, y_train, y_valid = train_test_split(x_train, y, test_size=settings.train_val_split, random_state=settings.seed)
 
     model.fit(x_train, y_train, epochs=2, batch_size=32)
 
