204809491936d80fab915be1dde27db35db04ca8
Stefan Taubert <stefco94@gmx.de>
2018-04-05 21:40:18 +0100

    .

--- src/XGBoostModel.py
+++ src/XGBoostModel.py
@@ -13,33 +13,24 @@ import time
 
 def run_Model():
     print("Run model...")
-    x_text = np.load(data_paths.x_text)
+    #x_text = np.load(data_paths.x_text)
+
+    x_text = pd.read_csv(data_paths.occurrences_train_gen)
+
     y = np.load(data_paths.y_ids)
 
     # species_count = np.load(data_paths.y_array).shape[1]
     
     x_train, x_valid, y_train, y_valid = train_test_split(x_text, y, test_size=settings.train_val_split, random_state=settings.seed)
-   
-    # Ist durch den RandomFinder behoben
-    # # Entferne Spezies aus dem Validierungsset, falls diese Spezies nicht im Trainingsset vorkommt
-    # indicies = []
-    # species_only_in_val = []
-    # for index in range(0, len(y_valid)):
-    #     item = y_valid[index]
-    #     if item not in y_train:
-    #         indicies.append(index)
-    #         species_only_in_val.append(item)
-    # x_valid = np.delete(x_valid, indicies, 0)
-    # y_valid = np.delete(y_valid, indicies)
-    # print("Dropped indicies from validationset because they are not in trainingsset:", indicies, "Count:", len(indicies))
-    
-    xg = XGBClassifier(
+
+    xg = XGBClassifier
+    (
         objective="multi:softmax",
         eval_metric="merror",
         random_state=settings.seed,
         n_jobs=-1,
         n_estimators=30,
-        predictor='gpu_predictor',
+        # predictor='gpu_predictor',
     )
 
     print("Fit model...")
