6570adf12b5cca38f320f265dfa7c0753a5e0662
Stefan Taubert <stefco94@gmx.de>
2018-04-14 13:42:03 +0200

    .

--- src/Analysis/data_paths.py
+++ src/Analysis/data_paths.py
@@ -6,6 +6,12 @@ root = root_lines[0]
 occurrences_train_gen = root + "occurrences_train_gen_" + str(settings.pixel_count) + ".csv"
 occurrences_test_gen = root + "occurrences_test_gen_" + str(settings.pixel_count) + ".csv"
 
+patch_train = root + "patchTrain"
+heatmaps = root + "analysis/Heatmaps/"
+
+occurrences_train = root + "occurrences_train.csv"
+occurrences_test = root + "occurrences_test.csv"
+
 max_values_species = root + "analysis/max_values_species.csv"
 species_channel_map_dir = root + "analysis/channel_maps/"
 channel_map_diff = root + "analysis/channel_map_diff.csv"
