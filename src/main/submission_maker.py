670b90248e2d88b065f1dba6d925f311000a8643
Stefan Taubert <stefco94@gmx.de>
2018-04-16 13:38:45 +0200

    .

--- src/main/submission_maker.py
+++ src/main/submission_maker.py
@@ -56,49 +56,6 @@ def make_submission():
     # with open(data_paths.species_map, 'rb') as f:
     #     y = pickle.load(f)
         
-# def make_submission2():
-#     print("Make submission...")    
-#     #x_text = np.load(data_paths.x_text)
-    
-#     x_text = pd.read_csv(data_paths.occurrences_train_gen)
-#     species = x_text.species_glc_id.unique()
-#     #y = x_text.species_glc_id
-#     x_text = x_text[['chbio_1', 'chbio_5', 'chbio_6','month', 'latitude', 'longitude']]
-
-#     y = np.load(data_paths.y_array)
-#     #species = np.load(data_paths.species_map)
-#     y_predicted = np.load(data_paths.prediction)
-#     x_train, x_valid, y_train, y_valid = train_test_split(x_text, y, test_size=train_val_split, random_state=seed)
-
-#     # print("Validationset rows after removing unique species:", len(x_valid.index))
-
-#     sol_rows = []
-
-#     for i in tqdm(range(len(y_valid))):
-#         current_pred_array = y_predicted[i]
-#         current_glc_id = i
-#         pred_r = rankdata(current_pred_array, method="ordinal")
-#         # absteigend sortieren
-#         pred_r = len(species) - pred_r + 1
-       
-#         i, = np.where(y_valid[i] == 1)
-#         assert len(i) == 1
-
-#         current_rank = int(pred_r[i])
-#         current_species = int(species[i])
-#         current_prediction = float(current_pred_array[i])
-
-#         sol_row = []
-#         sol_row.append(current_glc_id)
-#         sol_row.append(current_species)
-#         sol_row.append(current_prediction)
-#         sol_row.append(current_rank)
-
-#         sol_rows.append(sol_row)
-
-#     # <glc_id;species_glc_id;probability;rank>        
-#     result_ser = pd.DataFrame(sol_rows, columns = ['glc_id', 'species_glc_id', 'probability', 'rank'])
-#     result_ser.to_csv(data_paths.submission_val, index=False)
 
 def make_submission_for_current_training():
     print("Make submission...")
