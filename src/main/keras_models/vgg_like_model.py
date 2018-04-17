ceeeeb168350a59dd774dadc27b17083a03ceca7
Max Mauermann <max.mauermann@s2014.tu-chemnitz.de>
2018-04-17 11:07:24 +0200

    BatchNormalization beim Model

--- src/main/keras_models/vgg_like_model.py
+++ src/main/keras_models/vgg_like_model.py
@@ -15,67 +15,39 @@ import tensorflow as tf
 def get_model(output_shape, channel_count):
     model = Sequential()
 
-    model.add(Conv2D(input_shape=(channel_count, 64, 64),
-                     filters=64,
-                     activation='relu',
-                     kernel_size=(3, 3),
-                     strides=(1, 1),
-                     padding='same',
-                     data_format='channels_first'))
-    model.add(Conv2D(filters=64,
-                     activation='relu',
-                     kernel_size=(3, 3),
-                     strides=(1, 1),
-                     padding='same',
-                     data_format='channels_first'))
-    model.add(Conv2D(filters=64,
-                     activation='relu',
-                     kernel_size=(3, 3),
-                     strides=(1, 1),
-                     padding='same',
-                     data_format='channels_first'))
-    model.add(MaxPooling2D(pool_size=(2,2),
-                               data_format='channels_first'))
-    model.add(Conv2D(filters=128,
-                     activation='relu',
-                     kernel_size=(3, 3),
-                     strides=(1, 1),
-                     padding='same',
-                     data_format='channels_first'))
-    model.add(Conv2D(filters=128,
-                     activation='relu',
-                     kernel_size=(3, 3),
-                     strides=(1, 1),
-                     padding='same',
-                     data_format='channels_first'))
-    model.add(Conv2D(filters=128,
-                     activation='relu',
-                     kernel_size=(3, 3),
-                     strides=(1, 1),
-                     padding='same',
-                     data_format='channels_first'))
-    model.add(MaxPooling2D(pool_size=(2,2),
-                               data_format='channels_first'))
-    model.add(Conv2D(filters=256,
-                     activation='relu',
-                     kernel_size=(3, 3),
-                     strides=(1, 1),
-                     padding='same',
-                     data_format='channels_first'))
-    model.add(Conv2D(filters=256,
-                     activation='relu',
-                     kernel_size=(3, 3),
-                     strides=(1, 1),
-                     padding='same',
-                     data_format='channels_first'))
-    model.add(Conv2D(filters=256,
-                     activation='relu',
-                     kernel_size=(3, 3),
-                     strides=(1, 1),
-                     padding='same',
-                     data_format='channels_first'))
-    model.add(AveragePooling2D(pool_size=(16, 16),
-                                    data_format='channels_first'))
+    model.add(Conv2D(input_shape=(channel_count, 64, 64), filters=64, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(filters=64, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(filters=64, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(filters=64, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(MaxPooling2D(pool_size=(2,2), data_format='channels_first'))
+
+    model.add(Conv2D(filters=128, activation='relu', kernel_size=(3, 3),  strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(filters=128, activation='relu', kernel_size=(3, 3),  strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(filters=128, activation='relu', kernel_size=(3, 3),  strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(MaxPooling2D(pool_size=(2,2), data_format='channels_first'))
+
+    model.add(Conv2D(filters=256, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(filters=256, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(filters=256, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(MaxPooling2D(pool_size=(2,2), data_format='channels_first'))
+
+    model.add(Conv2D(filters=512, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(filters=512, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(filters=512, activation='relu', kernel_size=(3, 3), strides=(1, 1), padding='same', data_format='channels_first'))
+    model.add(BatchNormalization(axis=1))
+    model.add(AveragePooling2D(pool_size=(8, 8), data_format='channels_first'))
     model.add(Dropout(0.5))
     model.add(Flatten())
     model.add(Dense(units=output_shape, activation='softmax'))
