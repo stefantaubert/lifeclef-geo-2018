d4ee724f881f0b926a3715b8091c414757f5efe0
Stefan Taubert <stefco94@gmx.de>
2018-04-20 18:56:37 +0200

    .

--- src/module_support.py
+++ src/module_support.py
@@ -6,5 +6,5 @@ dirs = next(os.walk(dir_path))[1]
 
 for d in dirs:
     if not d.startswith("."):
-        dir_full_path = dir_path + "\\" + d
+        dir_full_path = dir_path + "/" + d
         sys.path.append(dir_full_path)
