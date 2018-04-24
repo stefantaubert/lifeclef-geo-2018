76d416f56cb9cfe075f2f6f06955211c7a0d96d9
Stefan Taubert <stefco94@gmx.de>
2018-04-24 09:33:00 +0200

    .

--- src/main/settings_main.py
+++ src/main/settings_main.py
@@ -9,8 +9,8 @@ TOP_N_SUBMISSION_RANKS = 100
 
 ### Gibt den Seed für den Split und das Training an.
 ### Contains the seed which is used to split the trainset and for training
-seed = 27 #4 hier ist bei 0.1 jede Spezies aus dem Validierungsset im Trainingsset vorhanden
+seed = 4 #27 #4 hier ist bei 0.1 jede Spezies aus dem Validierungsset im Trainingsset vorhanden
 
 ### Gibt das Split-Verhältnis von Trainings- und Validierungsset an.
 ### Contains the split ratio for trainset
-train_val_split = 0.3 #0.1
+train_val_split = 0.1
