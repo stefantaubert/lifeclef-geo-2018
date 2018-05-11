bfce856cda356d9e7bde996d8d9f1e2898793c76
Stefan Taubert <stefco94@gmx.de>
2018-05-11 23:34:06 +0200

    .

--- src/main/playground.py
+++ src/main/playground.py
@@ -1,2 +1,19 @@
-i = [x for x in range(2)]
-print(i)
+import multiprocessing
+import time
+import random
+
+def hello(n):
+    time.sleep(random.randint(1,3))
+    print("[{0}] Hello!".format(n))
+
+if __name__ == '__main__':
+    processes = [ ]
+    for i in range(10):
+        t = multiprocessing.Process(target=hello, args=(i,))
+        processes.append(t)
+        t.start()
+
+    for one_process in processes:
+        one_process.join()
+
+    print("Done!")
