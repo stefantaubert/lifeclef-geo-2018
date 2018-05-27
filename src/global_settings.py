833853a5bf77a54fb7dad8442664d9621337ae1b
Stefan Taubert <stefco94@gmx.de>
2018-05-27 20:01:07 +0200

    .

--- src/global_settings.py
+++ src/global_settings.py
@@ -2,7 +2,7 @@
 
 ### Gibt die Anzahl an Pixeln^2 pro Quadrant eines Bildes an.
 ### Minimum: 1 -> entspricht 4 mittlersten Pixel, Maximum: 32 entspricht allen Pixel
-pixel_count = 1
+pixel_count = 32
 
 ### Contains the threshold for the distances between each species for them to be in a group.
 ### Remarks: after changing this value, you have to run SimilarSpeciesExtractor and GroupExtractor again.
@@ -19,4 +19,4 @@ use_mean = 1
 min_occurence = 0
 
 ### Sets the count of digits for rounding the test and trainset in the analysis. Range: [0, 12]
-round_data_ndigits = 12
+round_data_ndigits = 2
